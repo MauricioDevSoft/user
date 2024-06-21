9a73b75b2fb491852e4d9651a0d567446298cc80
Mauricio <Mauricio@DESKTOP-40S0MI4>
2024-06-21 14:21:29 -0600

    bootstrap and component

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -3,11 +3,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ButtonsComponent } from '../../shared/buttons/buttons.component';
 import { InputComponent } from '../../shared/controls/input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormFieldComponent } from '../../shared/controls/form-field/form-field.component';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [ButtonsComponent, InputComponent, ReactiveFormsModule],
+  imports: [
+    ButtonsComponent,
+    InputComponent,
+    ReactiveFormsModule,
+    FormFieldComponent,
+  ],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css',
 })
